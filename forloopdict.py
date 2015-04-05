72c45997ad5936e29148512069b1fee30c79b608
unknown <cantony@CHLT23.chinada.local>
2015-04-05 17:18:15 -0400

    First Commit

---
+++ forloopdict.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 03 14:36:23 2015
+
+@author: cantony
+"""
+actors = {
+    "Kyle MacLachlan": "Dale Cooper",
+    "Sheryl Lee": "Laura Palmer",
+    "Lara Flynn Boyle": "Donna Hayward",
+    "Sherilyn Fenn" : "Audrey Horne"
+}
+
+for actor, character in actors.iteritems():
+    print 'name of the actor is {0} and the character played is {1}'.format(actor, character)
+    
+    
+    
